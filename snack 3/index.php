c4084a08dfc31c02703f1357b114d1dc8894fa8b
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-07-04 12:28:08 +0200

    arrays merged but elements not alternated

--- snack 3/index.php
+++ snack 3/index.php
@@ -1,7 +1,26 @@
 <?php 
-    $numbers = ["1", "2", "3", "4", "5"];
+    $numbers = [1, 2, 3, 4, 5];
 
     $letters = ["a", "b", "c", "d", "e"];
 
-    
+    function explodeAndMerge($array1, $array2){
+        return $mergedArray = [...$array1, ...$array2];
+    };
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack 3</title>
+    </head>
+
+    <body>
+        <h1>
+            <?php
+                var_dump(explodeAndMerge($numbers, $letters))  
+            ?>
+        </h1>
+    </body>
+</html>
