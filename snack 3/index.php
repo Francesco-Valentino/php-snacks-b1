249696d31432724c404aa7b922d8031384224bce
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-07-04 12:36:49 +0200

    app not working

--- snack 3/index.php
+++ snack 3/index.php
@@ -4,7 +4,13 @@
     $letters = ["a", "b", "c", "d", "e"];
 
     function explodeAndMerge($array1, $array2){
-        return $mergedArray = [...$array1, ...$array2];
+        $exploded1 = [...$array1];
+
+        $exploded2 = [...$array2];
+
+        $merged = [];
+
+        array_push($merged, $array1[0], $array2[0], $array1[1], $array2[1], $array1[2], $array2[2], $array1[3], $array2[3], $array1[4], $array2[4],);
     };
 ?>
 
